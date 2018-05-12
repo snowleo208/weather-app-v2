bec01ce14adad69bd7fa2c3e568a18ec54069660
snowleo208 <13027432+snowleo208@users.noreply.github.com>
2018-05-13 01:19:34 +0800

    add: express.js, API routes and logo files

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const axios = require('axios')
+require('dotenv').config();
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(bodyParser.json());
+
+const port = process.env.PORT || 3001;
+
+app.get('/api/ip', async function (req, res) {
+	let location = await axios.get('https://ipinfo.io?token=' + process.env.IPINFO_API_KEY)
+	.catch(err => console.log(err));
+	let weather = await axios.get("https://api.darksky.net/forecast/" + process.env.DARKSKY_API_KEY + "/" + location.data.loc)
+		.catch(err => console.log(err));
+	weather.data.location = location.data;
+	res.send(weather.data);
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,13 +1,39 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class Weather extends Component {
-  render() {
-    return (
-      <div className="u-container">
-        Weather App
-      </div>
-    );
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			location: {},
+			weather: {},
+			onTrigger: false,
+			onLoading: true
+		}
+		this.getData = this.getData.bind(this);
+	}
+	async getData() {
+		const location = await axios.get('/api/ip');
+		// const weather = await axios.get('/api/weather/');
+		console.log(location);
+		// console.log(weather);
+		// console.log(weather);
+		// this.setState({
+		//     location: location.data,
+		//     weather
+		// });
+	}
+	componentDidMount = () => {
+		console.log('new');
+		this.getData();
+	}
+	render() {
+		return (
+			<div className="u-container">
+			<h1>hava</h1>
+			</div>
+			);
+	}
 }
 
 export default Weather;
