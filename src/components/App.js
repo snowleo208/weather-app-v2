f5235054617efd732e75d2081753f6e9a0cdba75
snowleo208 <13027432+snowleo208@users.noreply.github.com>
2018-05-12 14:09:29 +0800

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import '../stylesheets/main.css';
+import Weather from './Weather';
+
+class App extends Component {
+  render() {
+    return (
+      <Weather />
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Weather.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Weather extends Component {
+  render() {
+    return (
+      <div className="u-container">
+        Weather App
+      </div>
+    );
+  }
+}
+
+export default Weather;
