03d7eaaf972523a40748e5041d9bcf26238158b4
snowleo208 <13027432+snowleo208@users.noreply.github.com>
2018-05-13 23:10:25 +0800

    add: ip check for heroku app

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(cors());
 app.use(bodyParser.urlencoded({
-    extended: true
+	extended: true
 }));
 
 app.use(bodyParser.json());
@@ -16,10 +16,15 @@ app.use(bodyParser.json());
 const port = process.env.PORT || 3001;
 
 app.get('/api/weather', async function (req, res) {
-	let location = await axios.get('https://ipinfo.io?token=' + process.env.IPINFO_API_KEY)
+	var ip = req.headers['x-forwarded-for'].split(',').pop() || 
+	req.connection.remoteAddress || 
+	req.socket.remoteAddress || 
+	req.connection.socket.remoteAddress;
+
+	let location = await axios.get('https://ipinfo.io/' + ip + '?token=' + process.env.IPINFO_API_KEY)
 	.catch(err => console.log(err));
 	let weather = await axios.get("https://api.darksky.net/forecast/" + process.env.DARKSKY_API_KEY + "/" + location.data.loc)
-		.catch(err => console.log(err));
+	.catch(err => console.log(err));
 	weather.data.location = location.data;
 	res.send(weather.data);
 });
@@ -28,5 +33,5 @@ app.listen(port, () => console.log(`Listening on port ${port}`));
 
 // Express only serves static assets in production
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('build'));
+	app.use(express.static('build'));
 }
