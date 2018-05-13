993e88158e09ddba46c6c60b103ff40ca6d9cd8f
snowleo208 <13027432+snowleo208@users.noreply.github.com>
2018-05-13 23:31:08 +0800

    add: create production version of backend API

--- server.js
+++ server.js
@@ -16,13 +16,20 @@ app.use(bodyParser.json());
 const port = process.env.PORT || 3001;
 
 app.get('/api/weather', async function (req, res) {
+	let location;
+	location = await axios.get('https://ipinfo.io/?token=' + process.env.IPINFO_API_KEY)
+	.catch(err => console.log(err));
+
+	if (process.env.NODE_ENV === 'production') {
 	var ip = req.headers['x-forwarded-for'].split(',').pop() || 
 	req.connection.remoteAddress || 
 	req.socket.remoteAddress || 
 	req.connection.socket.remoteAddress;
 
-	let location = await axios.get('https://ipinfo.io/' + ip + '?token=' + process.env.IPINFO_API_KEY)
+	location = await axios.get('https://ipinfo.io/' + ip + '?token=' + process.env.IPINFO_API_KEY)
 	.catch(err => console.log(err));
+	}
+
 	let weather = await axios.get("https://api.darksky.net/forecast/" + process.env.DARKSKY_API_KEY + "/" + location.data.loc)
 	.catch(err => console.log(err));
 	weather.data.location = location.data;
